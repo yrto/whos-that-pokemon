01bde53b996272573114802d8bdd7fa20ce2a0a0
Ayrton Mugnaini <ayrton.mugnaini@gmail.com>
2021-04-22 23:41:00 -0300

    Spacing fixes on mobile

--- src/whos-that-pokemon/WhosThatPokemonContainer.js
+++ src/whos-that-pokemon/WhosThatPokemonContainer.js
@@ -34,9 +34,9 @@ const WhosThatPokemonContainer = () => {
   }, [found]);
 
   return (
-    <div className="min-h-screen p-8 flex flex-col sm:p-12 poke-background">
+    <div className="min-h-screen p-6 flex flex-col sm:p-12 poke-background">
       {/* main container */}
-      <div className="w-full max-w-3xl mx-auto space-y-8 sm:space-y-12">
+      <div className="w-full max-w-3xl mx-auto space-y-6 sm:space-y-8 sm:space-y-12">
         {/* header */}
         <h1 className="text-center text-2xl sm:text-4xl font-bold uppercase text-white">
           {lang.h1}
@@ -49,71 +49,73 @@ const WhosThatPokemonContainer = () => {
           lang={lang}
         />
         {/* menus */}
-        <div className="font-black max-w-sm mx-auto rounded-xl p-4 bg-white">
-          {/* 1. guess */}
-          {menu === 1 && (
-            <PokeGuess
-              minMax={minMax}
-              misteryPokemon={misteryPokemon}
-              setMisteryPokemon={setMisteryPokemon}
-              guess={guess}
-              setGuess={setGuess}
-              fail={fail}
-              setFail={setFail}
-              setFound={setFound}
-              lang={lang}
-            />
-          )}
-          {/* 2. again */}
-          {menu === 2 && (
-            <PokeAgain
-              fail={fail}
-              setFail={setFail}
-              setFound={setFound}
-              setMenu={setMenu}
-              setMisteryPokemon={setMisteryPokemon}
-              lang={lang}
-            />
-          )}
-          {/* 3. range */}
-          {menu === 3 && (
-            <PokeRange
-              minMax={minMax}
-              setMinMax={setMinMax}
-              setMenu={setMenu}
-              setMisteryPokemon={setMisteryPokemon}
-              lang={lang}
-            />
-          )}
-        </div>
-        <div className="text-red-800 text-center text-sm">
-          <a href="https://github.com/yrto/whos-that-pokemon">
-            <MarkGithubIcon verticalAlign="middle" size="medium" />
-          </a>
-        </div>
-        <div className="text-red-800 text-center text-sm space-x-4">
-          <button
-            onClick={() => {
-              setLang(english);
-              localStorage.setItem("whos-that-language", "english");
-            }}
-            className={`font-bold focus:outline-none px-1 rounded ${
-              lang === english ? "bg-red-800 text-white" : ""
-            }`}
-          >
-            EN-US
-          </button>
-          <button
-            onClick={() => {
-              setLang(portuguese);
-              localStorage.setItem("whos-that-language", "portuguese");
-            }}
-            className={`font-bold focus:outline-none px-1 rounded ${
-              lang === portuguese ? "bg-red-800 text-white" : ""
-            }`}
-          >
-            PT-BR
-          </button>
+        <div className="px-4 space-y-6 sm:space-y-8">
+          <div className="font-black max-w-sm mx-auto rounded-xl p-4 bg-white">
+            {/* 1. guess */}
+            {menu === 1 && (
+              <PokeGuess
+                minMax={minMax}
+                misteryPokemon={misteryPokemon}
+                setMisteryPokemon={setMisteryPokemon}
+                guess={guess}
+                setGuess={setGuess}
+                fail={fail}
+                setFail={setFail}
+                setFound={setFound}
+                lang={lang}
+              />
+            )}
+            {/* 2. again */}
+            {menu === 2 && (
+              <PokeAgain
+                fail={fail}
+                setFail={setFail}
+                setFound={setFound}
+                setMenu={setMenu}
+                setMisteryPokemon={setMisteryPokemon}
+                lang={lang}
+              />
+            )}
+            {/* 3. range */}
+            {menu === 3 && (
+              <PokeRange
+                minMax={minMax}
+                setMinMax={setMinMax}
+                setMenu={setMenu}
+                setMisteryPokemon={setMisteryPokemon}
+                lang={lang}
+              />
+            )}
+          </div>
+          <div className="text-red-800 text-center text-sm">
+            <a href="https://github.com/yrto/whos-that-pokemon">
+              <MarkGithubIcon verticalAlign="middle" size="medium" />
+            </a>
+          </div>
+          <div className="text-red-800 text-center text-sm space-x-4">
+            <button
+              onClick={() => {
+                setLang(english);
+                localStorage.setItem("whos-that-language", "english");
+              }}
+              className={`font-bold focus:outline-none px-1 rounded ${
+                lang === english ? "bg-red-800 text-white" : ""
+              }`}
+            >
+              EN-US
+            </button>
+            <button
+              onClick={() => {
+                setLang(portuguese);
+                localStorage.setItem("whos-that-language", "portuguese");
+              }}
+              className={`font-bold focus:outline-none px-1 rounded ${
+                lang === portuguese ? "bg-red-800 text-white" : ""
+              }`}
+            >
+              PT-BR
+            </button>
+          </div>
         </div>
       </div>
     </div>
